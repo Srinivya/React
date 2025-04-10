25666bae246f38b67ca14d02e7969734ba6c38a8
Srinivya <srinivyat@gmail.com>
2025-04-10 13:03:40 +0530

    login

--- project/src/Components/ProtectiveRouter.js
+++ project/src/Components/ProtectiveRouter.js
@@ -2,12 +2,11 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
-
 const ProtectiveRouter = ({ children }) => {
   const { isLogin } = useContext(AuthContext);
 
   if (!isLogin) {
-    return <Navigate to="/login"  />;
+    return <Navigate to="/login" />;
   }
 
   return children;
